0083a8dd9e5a6dba8a4270061e4d38b22c13554d
Noura <noura.azeem@gmail.com>
2020-04-11 19:43:35 -0400

    moving

--- Exp_Analysis_4/assignment 2/Plot1.R
+++ Exp_Analysis_4/assignment 2/Plot1.R
@@ -1,20 +1,34 @@
-# Created by Noura Azeem
-# April 6, 2020
-# Plot 1 code
-# Assignment 1 
-# Exploratory Analysis
+# Exp_Analysis
+# Project 2
+# BY: Noura Azeem
+# Date: April 11
 
+# The overall goal of this assignment is to explore the National Emissions Inventory database 
+# and see what it say about fine particulate matter pollution in the United states over the 10-year period 1999–2008. 
+#You may use any R package you want to support your analysis.
 
-install.packages("data.table")
-library("data.table")
-library("dplyr")
-setwd("~/datasciencecoursera/Exp_Analysis_4/data")
+#Questions
+# You must address the following questions and tasks in your exploratory analysis. 
+# For each question/task you will need to make a single plot. Unless specified, 
+# you can use any plotting system in R to make your plot.
+library("ggplot2")
 
-elec1 <- read.table("household_power_consumption.txt", header = TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
-subset <- elec1[elec1$Date %in% c("1/2/2007", "2/2/2007"),]
+setwd("/Users/nouraazeem/datasciencecoursera/Exp_Analysis_4/data")
 
-setwd("~/datasciencecoursera/Exp_Analysis_4")
-# Plot 1 
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Question 1 ----
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the 
+# years 1999, 2002, 2005, and 2008.
+setwd("/Users/nouraazeem/datasciencecoursera/Exp_Analysis_4/assignment 2/")
+PM25.by.year <- tapply(NEI$Emissions, NEI$year, sum)
+
 png("plot1.png", width= 480, height=480)
-hist(as.numeric(subset$Global_active_power), col="red", main= "Global Active Power", xlab = "Global Active Power (kilowatts)")
+plot(names(PM25.by.year), PM25.by.year, type = "l",
+     xlab = "Year", ylab = expression("Total" ~ PM[2.5] ~ "Emissions (tons)"),
+     main = expression("Total US" ~ PM[2.5] ~ "Emissions by Year"))
 dev.off()
+
+# Total emissions have been decreasing in the United States from 1999 to 2008
